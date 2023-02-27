0dc35e1a57e9a622ea42607fab5b50e8992cf561
it20647650 <it20647650@my.sliit.lk>
2023-02-27 16:33:00 +0530

    modified some files

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 var cors = require("cors");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
 
 // database connection
 mongoose.connect(process.env.DATABASE, {
@@ -14,6 +16,19 @@ mongoose.connect(process.env.DATABASE, {
     useFindAndModify: false
 }).then(()=>console.log("DB Connected"))
 .catch((err)=>console.log(err));
+
+// middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: "5mb"}));
+app.use(bodyParser.urlencoded({
+    limit: "5mb",
+    extended: true
+}));
+app.use(cookieParser()); /*for authentication*/
+app.use(cors()); /*make request to backend*/
+
+// error middleware
+// app.use(errorHandler);
 
 // port
 const port = process.env.PORT || 9000
